882bda01b707d11ea215011bdf702cfa82e15d0b
Adeena Khan <145704470+adeenasif@users.noreply.github.com>
2025-01-24 10:15:19 +0500

    Add files via upload

---
+++ Lab 1/Q1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char* argv[]){
+	float highest = stof(argv[1]);
+	float SecHighest = 0;
+	
+	for(int i = 2; i < argc; i++){
+		if(stof(argv[i]) > highest){
+			SecHighest = highest;
+			highest = stof(argv[i]);
+		}
+		if (stof(argv[i]) != highest && stof(argv[i]) > SecHighest){
+			SecHighest = stof(argv[i]);
+		}
+	}
+	cout<<"Second Highest = "<<SecHighest;
+	
+	return 0;
+}
