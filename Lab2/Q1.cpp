c8db1c29c16da6248c3e5c15b7b9520a4186fd67
Adeena Khan <145704470+adeenasif@users.noreply.github.com>
2025-01-31 09:26:48 +0500

    Q1.cpp

--- Lab2/Q1.cpp
+++ Lab2/Q1.cpp
@@ -7,8 +7,8 @@ int main(int argc, char* argv[]){
 	
 	for(int i = 1; i < argc; i++){
 		int Num = stof(argv[i]);
-        ptr = &Num;
-        sum+= *ptr;
+        	ptr = &Num;
+        	sum+= *ptr;
 	}
 	
 	cout<<"Sum of all elements: "<<sum;
