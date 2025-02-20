053526788b88dafab106b5b143ddd3f5965b7688
Adeena Khan <145704470+adeenasif@users.noreply.github.com>
2025-02-20 20:44:38 +0500

    Update Q2.cpp

--- Lab 4/Q2.cpp
+++ Lab 4/Q2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
 
 class Polynomial {
@@ -7,32 +8,33 @@ private:
     int degree;  
 
 public:
-    Polynomial() : degree(0) {
+    Polynomial() : degree(0){  // default
+    	degree = 0;
         coefficients = new double[1]{0};
     }
 
-    Polynomial(int deg, const double* coeff) : degree(deg) {
+    Polynomial(int deg, const double* coeff) : degree(deg) {  // parameterized
+    	this->degree = degree;
         coefficients = new double[degree + 1];
         for (int i = 0; i <= degree; ++i) {
             coefficients[i] = coeff[i];
         }
     }
 
-    Polynomial(const Polynomial& other) : degree(other.degree) {
+    Polynomial(const Polynomial& other) {  // copy
+    	degree = other.degree;
         coefficients = new double[degree + 1];
         for (int i = 0; i <= degree; ++i) {
             coefficients[i] = other.coefficients[i];
         }
     }
 
-    // Move constructor
-    Polynomial(Polynomial&& other) noexcept : degree(other.degree), coefficients(other.coefficients) {
+    Polynomial(Polynomial&& other) noexcept : degree(other.degree), coefficients(other.coefficients) {// Move constructor
         other.coefficients = nullptr;
         other.degree = 0;
     }
 
-    // Destructor
-    ~Polynomial() {
+    ~Polynomial() { // Destructor 
         delete[] coefficients;
     }
 
@@ -40,9 +42,8 @@ public:
         return degree;
     }
 
-    //evaluate the polynomial
     double evaluate(double x) const {
-        double result = 0;
+        double result = 0.0;
         double xPower = 1;
 
         for (int i = 0; i <= degree; ++i) {
@@ -53,9 +54,8 @@ public:
         return result;
     }
 
-    // add another polynomial to the current one
     Polynomial add(const Polynomial& other) const {
-        int maxDegree = (degree > other.degree) ? degree : other.degree;
+        int maxDegree = max(degree, other.degree);
         double* newCoeffs = new double[maxDegree + 1]{};
 
         for (int i = 0; i <= degree; ++i) {
@@ -70,7 +70,6 @@ public:
         return result;
     }
 
-    // multiply another polynomial with the current one
     Polynomial multiply(const Polynomial& other) const {
         int newDegree = degree + other.degree;
         double* newCoeffs = new double[newDegree + 1]{};
@@ -85,14 +84,12 @@ public:
         delete[] newCoeffs;
         return result;
     }
+    
     void display() const {
         for (int i = degree; i >= 0; --i) {
-            if (i < degree && coefficients[i] >= 0) {
-                cout << "+";
-            }
             cout << coefficients[i];
-            if (i > 0) {
-                cout << "x^" << i << " ";
+            if (i != 0) {
+                cout << "x^" << i << " + ";
             }
         }
         cout << endl;
@@ -102,18 +99,34 @@ public:
 int main() {
     double coeffs1[] = {3, -3, 2, 1}; 
     Polynomial p1(3, coeffs1);
+    cout << "Polynomial 1: " << endl;
     p1.display();
 
-    double coeffs2[] = {3, 2};  
-    Polynomial p2(1, coeffs2);
+	cout << endl << endl;
+	
+	Polynomial p2 = p1;
+	cout << "Polynomial 2 copied from Polynomial 1: " << endl;
     p2.display();
-
-    Polynomial sum = p1.add(p2);
-    sum.display();
+    cout << endl << endl;
     
-    Polynomial product = p1.multiply(p2);
+    Polynomial p3 = move(p1);
+    cout << "Polynomial 3 moved from Polynomial 1: " << endl;
+    p3.display();
+    
+    cout << endl << endl;
+    cout << "p3 evaluated at x=2: " << p3.evaluate(2) << endl;
+    
+    cout << endl << endl;
+    
+    Polynomial product = p2.multiply(p3);
+    cout << "Polynomial after multiplying p2 and p3: " << endl;
     product.display(); 
     
-    cout << "p1 evaluated at x=2: " << p1.evaluate(2) << endl;
-    return 0;
+    cout << endl << endl;
+    
+    Polynomial added = p3.add(p2);
+    cout << "Polynomial after adding p2 and p3: " << endl;
+    added.display();
+	
+	return 0;
 }
