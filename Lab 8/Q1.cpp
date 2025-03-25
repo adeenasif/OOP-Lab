e59bc59479df819b32c14d8777cbe0c81902bd02
Adeena Khan <145704470+adeenasif@users.noreply.github.com>
2025-03-25 21:56:52 +0500

    Update Q1.cpp

--- Lab 8/Q1.cpp
+++ Lab 8/Q1.cpp
@@ -5,41 +5,101 @@ class Fraction {
 	int numerator;
 	int denominator;
 	
-	public:
-		Fraction(int num, int den) {
-			numerator = num;
-			denominator = den;
-			if (denominator < 0) {
-				numerator = -numerator;
-				denominator = -denominator;
-			}
-		}
-		
+	int findGCD(int a, int b) {
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        
 		void reducedFraction() {
-			if (numerator < denominator) {
-				min = numerator;
-			} else {
-				min = denominator;
-			}
-			while (min > 1) {
-				if ( numerator % min == 0 && denominator % min == -) {
-					break;
-				}
-				min--;
-			}
-			cout << "Reduced Numerator: " << numerator/min << endl;
-			cout << "Reduced Denominator: " << denominator/min << endl;
+            int g = findGCD((numerator),(denominator));
+            numerator /= g;
+            denominator /= g; 
+            
+            if (denominator < 0) {
+                numerator = -numerator;
+                denominator = -denominator;
+            }
+		}
+		
+	public:
+		Fraction(int num = 0, int den = 1) {
+            numerator = num;
+            denominator = den;
+            reducedFraction();
 		}
 		
 		Fraction operator+(Fraction const& obj) {
-			return Fraction((numerator*obj.denominator + obj.numerator*denominator)/ denominator*obj.denominator)
+			return Fraction((numerator*obj.denominator + obj.numerator*denominator), denominator*obj.denominator);
 		}
-		Fraction operator+(Fraction const& obj) {
-			return Fraction((numerator*obj.denominator + obj.numerator*denominator)/ denominator*obj.denominator)
+		Fraction operator-(Fraction const& obj) {
+			return Fraction((numerator*obj.denominator - obj.numerator*denominator), denominator*obj.denominator);
+		}
+		Fraction operator*(Fraction const& obj) {
+			return Fraction((numerator*obj.numerator), (denominator*obj.denominator));
+		}
+		Fraction operator/(Fraction const& obj) {
+			return Fraction(numerator*obj.denominator, denominator*obj.numerator);
+		}
+		bool operator==(const Fraction& obj) const {
+			return numerator == obj.numerator && denominator == obj.denominator;
+		}
+		bool operator!=(const Fraction& obj) const {
+			return numerator != obj.numerator && denominator != obj.denominator;
+		}
+		bool operator<(const Fraction& obj) const {
+			return numerator * obj.denominator < obj.numerator * denominator;
+        }
+		bool operator>(const Fraction& obj) const {
+			return numerator * obj.denominator > obj.numerator * denominator;
+        }
+		bool operator>=(const Fraction& obj) const {
+			return numerator * obj.denominator >= obj.numerator * denominator;
+        }
+		bool operator<=(const Fraction& obj) const {
+			return numerator * obj.denominator <= obj.numerator * denominator;
+        }
+        
+        friend ostream& operator<<(ostream& os, const Fraction& f) {
+        	os << f.numerator;
+        	if (f.denominator != 1) {
+        		os << "/" << f.denominator;
+			}
+			return os;
 		}
 		
+		friend istream& operator>>(istream& is, Fraction& f) {
+        	char symbol;
+            is >> f.numerator >> symbol >> f.denominator;
+            f.reducedFraction();
+            return is;
+		}
 };
 
-//3/2 + 2/3
-//9+4/6
-//13/6
+int main() {
+    Fraction f1, f2;
+    cout << "Enter two fractions (a/b): ";
+    cin >> f1 >> f2;
+
+    cout << "\nSimplified Fraction 1: " << f1 << endl;
+    cout << "Simplified Fraction 2: " << f2 << endl;
+    
+    cout << "\nArithmetic Operations : " << endl << endl;
+    cout << f1 << " + " << f2 << " = " << (f1 + f2) << endl;
+    cout << f1 << " - " << f2 << " = " << (f1 - f2) << endl;
+    cout << f1 << " * " << f2 << " = " << (f1 * f2) << endl;
+    cout << f1 << " / " << f2 << " = " << (f1 / f2) << endl;
+    
+    cout << "\nRelational Operators : " << endl << endl;
+    cout << f1 << " == " << f2 << " ? " << (f1 == f2 ? "True" : "False") << endl;
+    cout << f1 << " != " << f2 << " ? " << (f1 != f2 ? "True" : "False") << endl;
+    cout << f1 << " < " << f2 << " ? " << (f1 < f2 ? "True" : "False") << endl;
+    cout << f1 << " > " << f2 << " ? " << (f1 > f2 ? "True" : "False") << endl;
+    cout << f1 << " <= " << f2 << " ? " << (f1 <= f2 ? "True" : "False") << endl;
+    cout << f1 << " >= " << f2 << " ? " << (f1 >= f2 ? "True" : "False") << endl;
+
+    return 0;
+}
